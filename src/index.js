7ceb58dee5ee6f3deb6f64c06b6930a82d08352d
Deepak <deepakgunpal0@gmail.com>
2022-11-13 09:17:52 +0530

    cpu instance created

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const route = require('./route');
 require('dotenv/config');
+const cluster = require("cluster");
+const os = require("os");
+const numCPUs = os.cpus().length;
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,4 +22,13 @@ app.use('/api', route)
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
     .then(() => console.log('db connection established'));
 
-app.listen(PORT, () => console.log('listening on port: http://localhost:' + PORT));
+// For Master process
+if (cluster.isMaster) {
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) cluster.fork();
+    // This event is firs when worker died
+    cluster.on('exit', (worker, code, signal) => cluster.fork());
+}
+else {
+    app.listen(PORT, () => console.log('listening on port: http://localhost:' + PORT));
+}
